763669f2ad569952f68140b58f05a7dc247283d5
Roonglit Chareonsupkul <roonglit@gmail.com>
2020-04-29 18:52:21 +0700

    remove authenticatable

--- db/migrate/20181003072918_add_authenticatable_references_to_accounts.rb
+++ db/migrate/20181003072918_add_authenticatable_references_to_accounts.rb
@@ -2,10 +2,10 @@
 
 class AddAuthenticatableReferencesToAccounts < ActiveRecord::Migration[5.2]
   def change
-    add_reference :accounts,
-                  :authenticatable,
-                  polymorphic: true,
-                  index: true,
-                  null: false
+    # add_reference :accounts,
+    #               :authenticatable,
+    #               polymorphic: true,
+    #               index: true,
+    #               null: false
   end
 end
